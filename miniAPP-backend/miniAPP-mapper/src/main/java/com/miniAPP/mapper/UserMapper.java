279be91d652bf37bd5755d567bedc6264f30349c
lee yat-san <niuqiao2010@163.com>
2019-04-09 17:38:02 +0800

    Deploy to server

--- miniAPP-backend/miniAPP-mapper/src/main/java/com/miniAPP/mapper/UserMapper.java
+++ miniAPP-backend/miniAPP-mapper/src/main/java/com/miniAPP/mapper/UserMapper.java
@@ -2,8 +2,7 @@ package com.miniAPP.mapper;
 
 import com.miniAPP.pojo.User;
 import com.miniAPP.utils.MyMapper;
-import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
+
 public interface UserMapper extends MyMapper<User> {
 }
