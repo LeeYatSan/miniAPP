e7ab63baca1beb50ff2621fb92958706b5da8cae
hahaaichizi <44812060+hahaaichizi@users.noreply.github.com>
2019-04-17 15:00:47 +0800

    Update login.js

--- miniAPP-frontend/pages/login/login.js
+++ miniAPP-frontend/pages/login/login.js
@@ -1,22 +1,17 @@
 // pages/login/login.js
-
-const app=getApp()
-
+var app = getApp();
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    urlPath: app.globalData.urlPath,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
   },
 
   /**
@@ -68,37 +63,47 @@ Page({
 
   },
 
-  bindGetUserInfo: function (res) {
+  bindGetUserInfo: function(res) {
     var that = this;
     let info = res;
     console.log(info);
-    if (info.detail.userInfo) {
+    if(info.detail.userInfo) {
       console.log("点击了同意授权");
       wx.login({
         success: function (res) {
           if (res.code) {
+            console.log("请求"),
             wx.request({
-              url: that.data.urlPath,
+              url: app.globalData.urlPath + '/onRegister',
               method: 'POST',
+              //url:'http://localhost:8081/onLogin',
               data: {
                 code: res.code
-                // nickName: info.detail.userInfo.nickName,
-                // city: info.detail.userInfo.city,
-                // province: info.detail.userInfo.province,
-                // avatarUrl: info.detail.userInfo.avatarUrl
               },
               header: {
-                "Content-Type": "application/x-www-form-urlencoded" // 默认值
+                "Content-Type": "application/x-www-form-urlencoded"  // 默认值
               },
               success: function (res) {
+                console.log("成功");
                 var userinfo = {};
                 userinfo['id'] = res.data.id;
                 userinfo['nickName'] = info.detail.userInfo.nickName;
                 userinfo['avatarUrl'] = info.detail.userInfo.avatarUrl;
                 wx.setStorageSync('userinfo', userinfo);
+
+                console.log(res);
+                app.globalData.userID = res.data.data.id;
+                app.globalData.sessionToken = res.data.data.sessionToken;
+                
+                //跳转
+                console.log("跳转");
+                wx.switchTab({
+                  url: '../list/list',
+                })
+
               }
             })
-          } else {
+          }else {
             console.log("授权失败");
           }
         },
@@ -107,5 +112,21 @@ Page({
     } else {
       console.log("点击了拒绝授权");
     }
-  }
+  },
+  
+  queryUsreInfo: function () {
+    wx.request({
+      url: app.globalData.urlPath + '/onLogin',
+      data: {
+        openid: app.globalData.openid
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data);
+        getApp().globalData.userInfo = res.data;
+      }
+    });
+  },
 })
