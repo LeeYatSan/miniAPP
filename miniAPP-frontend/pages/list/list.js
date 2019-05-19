72898587ff194b8819821cb7925e9f3d2a7255c4
hahaaichizi <44812060+hahaaichizi@users.noreply.github.com>
2019-05-19 23:13:33 +0800

    Update list.js

--- miniAPP-frontend/pages/list/list.js
+++ miniAPP-frontend/pages/list/list.js
@@ -7,25 +7,33 @@ Page({
    */
   data: {
     index:0,
+    tagArray:[]
   },
 
   onLoad: function (options) {
     var that = this;
 
     //数据库得到标签数据
-//     wx.request({
-//       url: that.globalData.urlPath,
-//       data: {},
-//       header: {
-//         "Content-Type": "application/x-www-form-urlencoded" 
-//       },
-//       success:  function  (res)  {
-//         console.log(res.data)
-//         that.setData({
-//           tagArray: res.data //设置数据
-//         })
-//       }
-//     })
+    wx.request({
+      url: app.globalData.urlPath + '/getAllLabelsByUserID',
+      data: {
+      userID : app.globalData.userID,
+      sessionToken : app.globalData.sessionToken
+      },
+      method:'POST',
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded" 
+      },
+      success:  function  (res)  {
+        console.log('标签')
+        console.log(app.globalData.userID)
+        console.log(app.globalData.sessionToken)
+        console.log(res.data)
+        that.setData({
+          
+        })
+      }
+     })
 
     //数据库得到列表标签
     // wx.request({
@@ -46,11 +54,11 @@ Page({
     //   }
     // }),
 
-    that.setData(
-      {
-        tagArray: ['python', 'java', 'php']
-      }
-    )
+    // that.setData(
+    //   {
+    //     tagArray: ['python', 'java', 'php']
+    //   }
+    // )
   },
 
   bindPickerChange:function(e){
@@ -61,5 +69,11 @@ Page({
         index: e.detail.value
       }
     )
+  },
+
+  buttonBind:function(e){
+    wx.navigateTo({
+      url: '/pages/addList/addList',
+    })
   }
 })
