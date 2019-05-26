de53257b1d0bcc68dd346c1a62e9fc0b408f636a
hahaaichizi <44812060+hahaaichizi@users.noreply.github.com>
2019-05-26 23:29:47 +0800

    Update content.js

--- miniAPP-frontend/pages/content/content.js
+++ miniAPP-frontend/pages/content/content.js
@@ -1,1 +1,79 @@
 
+var app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list:[],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this
+    that.getPreCard()
+  },
+
+  getPreCard:function(){
+    var that = this
+    let prevPage = getCurrentPages()[getCurrentPages().length - 2] 
+    console.log(prevPage.data.index)
+    that.setData({
+      list:prevPage.data.list[prevPage.data.index],
+    })
+    console.log("查看")
+    console.log(that.data.list)
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
