a024e5daca9b13578843a302b8ac87fd252d8c29
hahaaichizi <44812060+hahaaichizi@users.noreply.github.com>
2019-05-28 10:13:14 +0800

    Update list.js

--- miniAPP-frontend/pages/list/list.js
+++ miniAPP-frontend/pages/list/list.js
@@ -7,12 +7,12 @@ Page({
    */
   data: {
     index:0,
+    labelIndex:0,
     tagArray:['全部'],
     listLength:0,
     labelLength:0,
     //list:[{title:'',content:'',label:0,img:''}],
     list:[],
-    index:0
   },
 
   onLoad: function (options) {
@@ -26,7 +26,7 @@ Page({
     wx.showNavigationBarLoading();
     var that = this;
     that.setData({
-      index:0
+      labelIndex:0
     })
     that.getCards()
     that.getLabels()
@@ -97,7 +97,7 @@ Page({
     console.log('picker发送选择改变，携带值为', e.detail.value)
     that.setData(
       {
-        index: e.detail.value
+        labelIndex: e.detail.value
       }
     )
     wx.request({
@@ -105,7 +105,7 @@ Page({
       data:{
         userID:app.globalData.userID,
         sessionToken:app.globalData.sessionToken,
-        labelContent:that.data.tagArray[that.data.index]
+        labelContent:that.data.tagArray[that.data.labelIndex]
       },
       method: 'POST',
       header: {
@@ -123,7 +123,7 @@ Page({
 
   buttonBind:function(e){
     wx.navigateTo({
-      url: '/pages/addList/addList',
+      url: '/pages/addList/addList?num=0',
     })
   },
 
