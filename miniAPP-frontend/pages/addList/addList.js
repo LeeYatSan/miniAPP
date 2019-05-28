b3d329f36726b164c35178aaaee4089dc6adf8d1
hahaaichizi <44812060+hahaaichizi@users.noreply.github.com>
2019-05-28 10:10:40 +0800

    Update addList.js

--- miniAPP-frontend/pages/addList/addList.js
+++ miniAPP-frontend/pages/addList/addList.js
@@ -13,7 +13,30 @@ Page({
     images: '',//图片
     labelContent: '',//标签
     photoData: '',
-    formId:''
+    formId:'',
+    num:0,
+    cardId:0,
+    img_ocr:''
+  },
+
+  onLoad:function(res){
+    var that = this
+    that.setData({
+      num:res.num
+    })
+    console.log(that.data.num)
+    if(that.data.num==1){
+      console.log('准备修改')
+      let prevPage = getCurrentPages()[getCurrentPages().length - 2]
+      that.setData({
+        title:prevPage.data.list.card.title,
+        content:prevPage.data.list.card.content,
+        labelContent: prevPage.data.list.labels,
+        images: prevPage.data.url,
+        cardId: prevPage.data.list.card.cardId,
+        photoData: prevPage.data.list.card.picUrl
+      })
+    }
   },
 
   submitInfo: function (res) {
@@ -97,6 +120,49 @@ Page({
     })
   },
 
+  imgOCR:function(e){
+    var that = this
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],// 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'],// 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        console.log(res)
+        wx.uploadFile({
+          url: app.globalData.urlPath + '/uploadPhoto',
+          filePath: res.tempFilePaths[0],//要上传文件资源的路径 String类型 
+          name: 'photo',
+          header: {
+            "Content-Type": "multipart/form-data"
+          },
+          formData: {
+            'sessionToken': app.globalData.sessionToken,
+            'userID': app.globalData.userID,
+            'ocr': "true"
+          },
+          success: function (res) {
+            console.log(res)
+            if (res.statusCode == 200) {
+              var data = JSON.parse(res.data)
+              data = JSON.parse(data.data)
+              console.log(data)
+              that.setData({
+                content: data.ocrText
+              })
+            }
+            if (res.statusCode != 200) {
+              wx.showModal({
+                title: '提示',
+                content: '提取文字失败',
+                confirmText: '确定',
+              })
+            }
+          }
+        })
+      },
+    })
+  },
+
   submit: function (e) {
     var that = this
     if (that.data.titleCount <= 0 || that.data.titleCount > 20 || that.data.contentCount <= 0 || that.data.contentCount > 200) {
@@ -114,33 +180,60 @@ Page({
         mask: true,
         duration: 500
       })
-      console.log(that.data.title)
-      console.log(that.data.content)
-      console.log(that.data.photoData)
-      console.log(that.data.labelContent)
-      console.log(that.data.formId)
-      wx.request({
-        url: app.globalData.urlPath + '/saveCard',
-        data: {
-          userID: app.globalData.userID,
-          sessionToken: app.globalData.sessionToken,
-          title: that.data.title,
-          content: that.data.content,
-          picUrl: that.data.photoData,
-          labelContent: that.data.labelContent,
-          formID:that.data.formId
-        },
-        method: 'POST',
-        header: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        },
-        success: function (res) {
-          console.log(res)
-          wx.switchTab({
-            url: '../list/list',
-          })
-        }
-      })
+      if(that.data.num==0){
+        wx.request({
+          url: app.globalData.urlPath + '/saveCard',
+          data: {
+            userID: app.globalData.userID,
+            sessionToken: app.globalData.sessionToken,
+            title: that.data.title,
+            content: that.data.content,
+            picUrl: that.data.photoData,
+            labelContent: that.data.labelContent,
+            formID:that.data.formId
+          },
+          method: 'POST',
+          header: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          success: function (res) {
+            console.log(res)
+            wx.switchTab({
+              url: '../list/list',
+            })
+          }
+        })
+      }
+      if(that.data.num==1){
+        console.log(that.data.title)
+        console.log(that.data.content)
+        console.log(that.data.photoData)
+        console.log(that.data.labelContent)
+        console.log(that.data.cardId)
+        wx.request({
+          url: app.globalData.urlPath + '/editCard',
+          data: {
+            userID: app.globalData.userID,
+            sessionToken: app.globalData.sessionToken,
+            cardId:that.data.cardId,
+            title: that.data.title,
+            content: that.data.content,
+            picUrl: that.data.photoData,
+            labelContent: that.data.labelContent,
+            formID: that.data.formId
+          },
+          method: 'POST',
+          header: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          success: function (res) {
+            console.log(res)
+            wx.switchTab({
+              url: '../list/list',
+            })
+          }
+        })
+      }
     }
   }
 })
