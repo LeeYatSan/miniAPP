f966b057dabd5d2eb69f862b0b714b877bbf14b2
Wingrez <31106425+wingrez@users.noreply.github.com>
2019-05-30 19:02:43 +0800

    更新

--- miniAPP-frontend/pages/cardDetail/cardDetail.js
+++ miniAPP-frontend/pages/cardDetail/cardDetail.js
@@ -72,7 +72,7 @@ Page({
         },
         method: 'GET',
         header: {
-          'content-type': 'application/json'
+          "Content-Type": "application/x-www-form-urlencoded"
         },
         success: function (res) {
           that.setData({
@@ -357,7 +357,7 @@ Page({
       },
       method: 'POST',
       header: {
-        'content-type': 'application/json'
+        "Content-Type": "application/x-www-form-urlencoded"
       },
       success: function (res) {
         console.log(res.data);
@@ -381,7 +381,7 @@ Page({
       },
       method: 'GET',
       header: {
-        'content-type': 'application/json'
+        "Content-Type": "application/x-www-form-urlencoded"
       },
       success: function (res) {
         console.log(res.data);
@@ -404,7 +404,7 @@ Page({
       },
       method: 'GET',
       header: {
-        'content-type': 'application/json'
+        "Content-Type": "application/x-www-form-urlencoded"
       },
       success: function (res) {
         console.log(res.data);
@@ -430,7 +430,7 @@ Page({
             },
             method: 'POST',
             header: {
-              'content-type': 'application/json'
+              "Content-Type": "application/x-www-form-urlencoded"
             },
             success: function (res) {
               console.log(res.data);
@@ -479,8 +479,7 @@ Page({
       },
       method: 'POST',
       header: {
-        'content-type': 'application/json'
-        // 'content-type': 'application/x-www-form-urlencoded'
+        "Content-Type": "application/x-www-form-urlencoded"
       },
       success: function (res) {
         console.log(res.data);
@@ -547,8 +546,7 @@ Page({
       },
       method: 'POST',
       header: {
-        'content-type': 'application/json'
-        // 'content-type': 'application/x-www-form-urlencoded'
+        "Content-Type": "application/x-www-form-urlencoded"
       },
       success: function (res) {
         console.log(res.data);
