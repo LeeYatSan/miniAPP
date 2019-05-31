ec834a077fa82f296d784dc477f8d9f2423be613
biglawine <dooky@foxmail.com>
2019-05-31 16:40:58 +0800

    me页面更新+bug修复

--- miniAPP-frontend/pages/me/me.js
+++ miniAPP-frontend/pages/me/me.js
@@ -5,14 +5,26 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userInfo:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this;
+    wx.login({
+      success: function () {
+        wx.getUserInfo({
+          success: function (res) {
+            console.log(res.userInfo);
+            that.setData({
+              userInfo: res.userInfo,
+            });
+          }
+        })
+      }
+    });
   },
 
   /**
